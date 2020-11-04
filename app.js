e777521de4c9897f41e45ff5b1dedfcc1f35ae84
tac <tacccccc21@gmail.com>
2020-11-04 17:54:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -24,5 +24,12 @@ addTask.addEventListener('submit', e => {
       createTodoList(task);
       // タスクに入力した文字をクリア
       addTask.reset();
+      // 削除機能
+list.addEventListener('click', e => {
+  if (e.target.classList.contains('delete')){
+      e.target.parentElement.remove();
   }
+
+
+  
 });
