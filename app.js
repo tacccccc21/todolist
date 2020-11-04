a6873c3d0414b2abdcd0b0ef942ca5ace539dc01
tac <tacccccc21@gmail.com>
2020-11-04 17:50:36 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const addTask = document.querySelector('.add');
 const list = document.querySelector('.todos');
+
+const createTodoList = task => {
+  // HTML テンプレートを生成
+  const html = `
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${task}</span>
+      <i class="far fa-trash-alt delete"></i>
+  </li>
+  `;
+
+  list.innerHTML += html;
+}
