714cd7b9e265328aa7462a0067a8031b3537ddcb
tac <tacccccc21@gmail.com>
2020-11-04 17:50:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -12,3 +12,17 @@ const createTodoList = task => {
 
   list.innerHTML += html;
 }
+
+addTask.addEventListener('submit', e => {
+  // デフォルトのイベントを無効
+  e.preventDefault();
+
+  // タスクに入力した値を空白を除外して格納
+  const task = addTask.add.value.trim();
+  if(task.length) {
+      // Todo List の HTML を作成
+      createTodoList(task);
+      // タスクに入力した文字をクリア
+      addTask.reset();
+  }
+});
